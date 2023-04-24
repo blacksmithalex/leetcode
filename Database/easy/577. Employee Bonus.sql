289b9dc40cffe26a9ed02385cc226c7f26a178a1
blacksmithalex <blacksmithalex@mail.ru>
2023-04-24 19:50:30 +0300

    24apr23

---
+++ Database/easy/577. Employee Bonus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+    Customers.name AS Customers
+FROM
+    Customers
+LEFT JOIN 
+    Orders 
+ON Customers.id = Orders.customerId
+WHERE Orders.customerId IS NULL 
